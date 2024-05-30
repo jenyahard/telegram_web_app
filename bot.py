da91418d059de77d7bb5cc9517c167ff9c6639d0
jenyahard <e.masliuk@glory.bet>
2024-05-31 00:07:05 +0300

    some

--- bot.py
+++ bot.py
@@ -1,13 +1,8 @@
-import telebot
-from threading import Thread
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
-import subprocess
-
 # Замените значения YOUR_BOT_TOKEN на ваш токен бота
 import telebot
-from threading import Thread
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
-import subprocess
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, CallbackQuery
+
+
 
 # Замените значения YOUR_BOT_TOKEN на ваш токен бота
 bot_token = '6758909655:AAFuHmzMBqrc7_B6OiuwidkAcfgGqeSkp3o'
@@ -19,11 +14,6 @@ local_ip = 'https://jenyahard.github.io/telegram_web_app/'
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Нажмите кнопку /webapp чтобы перейти в веб приложение.")
-
-# Обработчик команды /webapp
-@bot.message_handler(commands=['webapp'])
-def send_webapp_link(message):
     markup = InlineKeyboardMarkup()
     web_app_button = InlineKeyboardButton(
         text="Открыть WebApp",
@@ -32,14 +22,17 @@ def send_webapp_link(message):
     markup.add(web_app_button)
     bot.send_message(message.chat.id, "Нажмите кнопку ниже, чтобы открыть WebApp.", reply_markup=markup)
 
+# Обработчик callback_query
+@bot.callback_query_handler(func=lambda call: True)
+def handle_callback(call: CallbackQuery):
+    data = call.data  # данные, полученные из веб-приложения
+    bot.send_message(call.message.chat.id, f"Получены данные из WebApp: {data}")
+
+
 def run_bot():
     bot.polling()
 
-if __name__ == '__main__':
-    # Запуск Flet веб-приложения в отдельном потоке
-    flet_thread = Thread(target=lambda: subprocess.run(["python", "webapp.py"]))
-    flet_thread.setDaemon(True)
-    flet_thread.start()
 
+if __name__ == '__main__':
     # Запуск бота
     run_bot()
